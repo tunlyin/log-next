5ea7cd9f21900d8cb7e9db272eda30c8da1968e3
tunlyin <tunlyinaung.mmocc@gmail.com>
2024-01-26 13:20:41 +0630

    logger file location change

--- logger.js
+++ logger.js
@@ -6,7 +6,7 @@ const logger = winston.createLogger({
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.File({ 
-        filename: "app.log"
+        filename: "/var/log/log-next/app.log"
     }),
   ],
 });
